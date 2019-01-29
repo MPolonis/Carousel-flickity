345e05ae862ecafc9cbf33ff6187659dcf995f56
Marcin Polonis <marcin.polonis@o2.pl>
2019-01-29 20:44:58 +0100

    Remove unnecessery code lines from script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-var elem = document.querySelector('.main-carousel');
-var flkty = new Flickity( elem, {
-  // options
-  cellAlign: 'left',
-  contain: true
-});
-
-// element argument can be a selector string
-//   for an individual element
 var flkty = new Flickity( '.carousel', {
   // options
   hash: true,
