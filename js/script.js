e8f6fd024c55f6387c75fa301f36e37041170f75
Marcin Polonis <marcin.polonis@o2.pl>
2019-01-29 20:27:14 +0100

    Add button

--- js/script.js
+++ js/script.js
@@ -11,3 +11,7 @@ var flkty = new Flickity( '.carousel', {
   // options
   hash: true,
 });
+var previousWrappedButton = document.querySelector('.button--next-wrapped');
+previousWrappedButton.addEventListener('click', function () {
+  flkty.next(true);
+});
