aa6e9ef8c0e4be10432fb752f3ebecf4b665a371
Marcin Polonis <marcin.polonis@o2.pl>
2019-01-29 21:20:44 +0100

    Add event scroll

--- js/script.js
+++ js/script.js
@@ -6,3 +6,9 @@ var previousWrappedButton = document.querySelector('.button--next-wrapped');
 previousWrappedButton.addEventListener('click', function () {
   flkty.next(true);
 });
+var progressBar = document.querySelector('.progress-bar')
+
+flkty.on('scroll', function (progress) {
+  progress = Math.max(0, Math.min(1, progress));
+  progressBar.style.width = progress * 100 + '%';
+});
