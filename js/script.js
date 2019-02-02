39037cc4e5b94bcbbbf8e09c7da7ccb1657671b2
Marcin Polonis <marcin.polonis@o2.pl>
2019-02-02 17:34:22 +0100

    Add mustache.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+'use strict';
+(function () {
+
+
+var templateItem = document.getElementById('template-item').innerHTML;
+Mustache.parse(templateItem);
+
+var listItems = '';
+
+for (var i = 0; i < slides.length; i++) {
+  listItems += Mustache.render(templateItem, slides[i]);
+}
+
+  var fullSlidesList = Mustache.render(templateItem, { results: listItems });
+  var finish = document.querySelector('.carousel');
+  finish.insertAdjacentHTML('beforeend', fullSlidesList);
+})();
+
+
+
+
+
+
+
 var flkty = new Flickity( '.carousel', {
   // options
   hash: true,
