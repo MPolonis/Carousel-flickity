4e4ffc9db9a9f26201983829a3efb78fa0aa3e2b
Marcin Polonis <marcin.polonis@o2.pl>
2019-02-03 16:32:28 +0100

    Improve restart button

--- js/script.js
+++ js/script.js
@@ -2,9 +2,9 @@ var flkty = new Flickity( '.carousel', {
   // options
   hash: true,
 });
-var previousWrappedButton = document.querySelector('.button--next-wrapped');
-previousWrappedButton.addEventListener('click', function () {
-  flkty.next(true);
+var restartButton = document.querySelector('.button-restart');
+restartButton.addEventListener('click', function () {
+  flkty.select(0);
 });
 var progressBar = document.querySelector('.progress-bar')
 
